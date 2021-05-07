2c022bfaacdd5d5fd4d0f0d7b15c1209796ee0d1
Phil <philip.tyler@mt.com>
2021-05-07 18:25:47 +0100

    Base56 Test Class Added - No Tests Yet.

---
+++ DataConversionTools.Tests/Base64ToolsTests.cs
@@ -0,0 +1,21 @@
+﻿// <summary>
+// Project Name         :   Data Conversion Tools
+// Project Description  :   A simple C# Library to for working with Binary, Hex and Strings
+// Project Website      :   https://github.com/laidbackcoder/DataConversionTools
+// License              :   MIT License
+// License URL          :   https://github.com/laidbackcoder/DataConversionTools/blob/master/LICENSE
+// </summary>
+using NUnit.Framework;
+using PT.DataConversionTools.Exceptions;
+
+namespace PT.DataConversionTools.Tests
+{
+    /// <summary>
+	/// Test Fixtures for the <see cref="Base64Tools" /> class.
+	/// </summary>
+	[TestFixture]
+    public class Base64ToolsTests
+    {
+        //TODO: Implement Tests for Base64 Helper Methods
+    }
+}
